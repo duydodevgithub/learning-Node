766e925f164799af4c21e86c21b215b168d073be
DuyDo <duydo.work@gmail.com>
2017-08-21 21:47:23 -0500

    test

--- demo-app-faker/listProducts.js
+++ demo-app-faker/listProducts.js
@@ -1,7 +1,10 @@
 var faker = require("faker");
+console.log("=======================");
+console.log("WELCOME TO MY SHOP!");
+console.log("=======================")
+
 for(i = 0; i < 10; i++) {
     var product = faker.commerce.product();
     var price = faker.commerce.price();
-
     console.log("Product: " + product + " --- " + "Price: " + price);
 }
